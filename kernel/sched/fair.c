ca6b788863cd9c6615354c1a620708a8697247b6
Wei Wang <wvw@google.com>
2020-01-22 12:01:27 -0800

    sched: restrict iowait boost to tasks with prefer_idle
    Currently iowait doesn't distinguish background/foreground tasks and we
    have seen cases where a device run to high frequency unnecessarily when
    running some background I/O. This patch limits iowait boost to tasks with
    prefer_idle only. Specifically, on Pixel, those are foreground and top
    app tasks.
    
    Bug: 130308826
    Test: Boot and trace
    Change-Id: I2d892beeb4b12b7e8f0fb2848c23982148648a10
    Signed-off-by: Wei Wang <wvw@google.com>
    Signed-off-by: Lau <laststandrighthere@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5075,6 +5075,7 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	 */
 	schedtune_enqueue_task(p, cpu_of(rq));
 #endif
+	bool prefer_idle = schedtune_prefer_idle(p) > 0;
 
 	/*
 	 * The code below (indirectly) updates schedutil which looks at
@@ -5089,8 +5090,8 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	 * utilization updates, so do it here explicitly with the IOWAIT flag
 	 * passed.
 	 */
-	if (p->in_iowait)
-		cpufreq_update_this_cpu(rq, SCHED_CPUFREQ_IOWAIT);
+	if (p->in_iowait && prefer_idle)
+		cpufreq_update_util(rq, SCHED_CPUFREQ_IOWAIT);
 
 	for_each_sched_entity(se) {
 		if (se->on_rq)
