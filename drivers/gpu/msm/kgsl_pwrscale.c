bf80c7c3a9e3a0cc61be66c24ccd64ef7255ce08
ayushspidy <ayushspidy.git@gmail.com>
2021-08-09 18:18:14 +0000

    gpu/msm: Reduce polling interval

--- drivers/gpu/msm/kgsl_pwrscale.c
+++ drivers/gpu/msm/kgsl_pwrscale.c
@@ -876,7 +876,7 @@ int kgsl_pwrscale_init(struct device *dev, const char *governor)
 	profile->initial_freq =
 		pwr->pwrlevels[pwr->default_pwrlevel].gpu_freq;
 	/* Let's start with 10 ms and tune in later */
-	profile->polling_ms = 10;
+	profile->polling_ms = 5;
 
 	/* do not include the 'off' level or duplicate freq. levels */
 	for (i = 0; i < (pwr->num_pwrlevels - 1); i++)
