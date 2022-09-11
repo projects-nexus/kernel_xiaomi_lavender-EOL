eac4d842262962f806c704df7f4d4ae5099a0115
ImSpiDy <SpiDy2713@gmail.com>
2022-09-11 04:17:53 +0000

    cpufreq: schedutil: tune default rate limit us [3]
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -746,11 +746,11 @@ static int sugov_init(struct cpufreq_policy *policy)
                 tunables->up_rate_limit_us = LATENCY_MULTIPLIER / 2;
 
 	if (cpumask_test_cpu(policy->cpu, cpu_perf_mask)) {
-                tunables->down_rate_limit_us = LATENCY_MULTIPLIER * 10;
+                tunables->down_rate_limit_us = LATENCY_MULTIPLIER * 25;
         }
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask)) {
-		tunables->down_rate_limit_us = LATENCY_MULTIPLIER * 30;
+		tunables->down_rate_limit_us = LATENCY_MULTIPLIER * 15;
         }
 		lat = policy->cpuinfo.transition_latency / NSEC_PER_USEC;
 		if (lat) {
