b1a0d212fa0d8a09768a786a54ff987bb84c604a
ImSpiDy <SpiDy2713@gmail.com>
2023-01-08 17:38:48 +0000

    linux: binfmts: remove perfhal 1.0 from blocklist
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -129,7 +129,6 @@ static inline bool task_is_booster(struct task_struct *tsk)
 	       !strcmp(comm, "power@1.0-servi") ||
 	       !strcmp(comm, "power@1.2-servi") ||
 	       !strcmp(comm, "power@1.3-servi") ||
-	       !strcmp(comm, "perf@1.0-servic") ||
 	       !strcmp(comm, "init.qcom.post_");
 }
 
